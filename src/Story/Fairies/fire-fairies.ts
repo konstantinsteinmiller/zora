d6320f0e491a549d31aae2063cb06df35a1e3d20
Konstantin Steinmiller <konstantin.steinmiller@gmail.com>
2025-04-30 00:48:32 +0200

    feature: - added dragon-middle and dragon-young - improved level up logic - clean up stuff

--- src/Story/Fairies/fire-fairies.ts
+++ src/Story/Fairies/fire-fairies.ts
@@ -1,6 +1,7 @@
 import { BLAZING_FEATHERS, BLINDING_FIRE, FIERY_EVADE, FIRE_BREEZE, SPARK } from '@/Story/Spells/fire.ts'
 import { ELEMENTS } from '@/utils/enums.ts'
 import { calcStatGrowth } from '@/utils/fairy.ts'
+import { ref } from 'vue'
 
 export const FIRE_HARPY = {
   name: 'Harpire',
@@ -39,6 +40,88 @@ export const FIRE_HARPY = {
   spells: [BLAZING_FEATHERS, FIRE_BREEZE],
   passiveSpells: [BLINDING_FIRE, FIERY_EVADE],
 }
+
+const dragir = ref(null)
+const dragorin = ref(null)
+const dragoire = ref(null)
+export const FIRE_DRAGON_YOUNG = {
+  name: 'Dragir',
+  id: 'fire_dragon_young',
+  modelPath: '/models/dragon-young/dragon-young.fbx',
+  description: 'A young fire dragon fairy. Its weak but willing to learn.',
+  element: ELEMENTS.FIRE,
+  tier: 0,
+  level: 5,
+  xp: 0,
+  nextLevelXp: 10,
+  statGrowthPerLevel: {
+    hp: calcStatGrowth(200, 0),
+    power: calcStatGrowth(0.5, 0),
+    defense: calcStatGrowth(14, 0),
+    speed: calcStatGrowth(0.35, 0),
+    special: calcStatGrowth(0.35, 0),
+  },
+  evolutionsList: [null, dragorin],
+  statsGrowthVisual: {
+    hp: 5,
+    defense: 3,
+    speed: 2,
+    special: 4,
+  },
+  stats: {
+    name: 'Dragir',
+    hp: 25,
+    previousHp: 25,
+    maxHp: 25,
+    damage: 7,
+    defense: 6,
+    speed: 3,
+    special: 3,
+  },
+  spells: [SPARK],
+  passiveSpells: [],
+}
+dragir.value = FIRE_DRAGON_YOUNG
+
+export const FIRE_DRAGON_MIDDLE = {
+  name: 'Dragorin',
+  id: 'fire_dragon_middle',
+  modelPath: '/models/dragon-middle/dragon-middle.fbx',
+  description: "An adult fire dragon fairy. The power grows with it's flame on his tail.",
+  element: ELEMENTS.FIRE,
+  tier: 1,
+  level: 5,
+  xp: 0,
+  nextLevelXp: 10,
+  statGrowthPerLevel: {
+    hp: calcStatGrowth(260, 1),
+    power: calcStatGrowth(0.75, 1),
+    defense: calcStatGrowth(18, 1),
+    speed: calcStatGrowth(0.25, 1),
+    special: calcStatGrowth(0.5, 1),
+  },
+  evolutionsList: [dragir, dragoire],
+  statsGrowthVisual: {
+    hp: 4,
+    defense: 5,
+    speed: 1,
+    special: 2,
+  },
+  stats: {
+    name: 'Dragorin',
+    hp: 25,
+    previousHp: 25,
+    maxHp: 25,
+    damage: 7,
+    defense: 6,
+    speed: 3,
+    special: 3,
+  },
+  spells: [SPARK],
+  passiveSpells: [],
+}
+dragorin.value = FIRE_DRAGON_MIDDLE
+
 export const FIRE_DRAGON_OLD = {
   name: 'Dragoire',
   id: 'fire_dragon_old',
@@ -56,6 +139,7 @@ export const FIRE_DRAGON_OLD = {
     speed: calcStatGrowth(0.25, 2),
     special: calcStatGrowth(0.5, 2),
   },
+  evolutionsList: [dragorin, null],
   statsGrowthVisual: {
     hp: 5,
     defense: 3,
@@ -75,5 +159,6 @@ export const FIRE_DRAGON_OLD = {
   spells: [SPARK],
   passiveSpells: [],
 }
+dragoire.value = FIRE_DRAGON_OLD
 
-export default [FIRE_HARPY, FIRE_DRAGON_OLD]
+export default [FIRE_HARPY, FIRE_DRAGON_YOUNG, FIRE_DRAGON_MIDDLE, FIRE_DRAGON_OLD]
--- src/Story/Fairies/nature-fairies.ts
+++ src/Story/Fairies/nature-fairies.ts
@@ -1,6 +1,8 @@
 import { FLOWER_POWER, NATURES_SHIELD } from '@/Story/Spells/nature.ts'
+import type { Fairy } from '@/types/fairy.ts'
 import { ELEMENTS } from '@/utils/enums.ts'
 import { calcStatGrowth } from '@/utils/fairy.ts'
+import { ref, type Ref } from 'vue'
 
 export const NATURE_BUTTERFLY_MIDDLE = {
   name: 'Dandalina',
@@ -39,4 +41,82 @@ export const NATURE_BUTTERFLY_MIDDLE = {
   passiveSpells: [NATURES_SHIELD],
 }
 
-export default [NATURE_BUTTERFLY_MIDDLE]
+const Mushiddle: Ref<Fairy | null> = ref(null)
+export const NATURE_MUSHROOM_YOUNG = {
+  name: 'Mushyu',
+  id: 'nature_mushroom_young',
+  modelPath: '/models/mushroom-young/mushroom-young.fbx',
+  description: 'A nature fairy, known for hiding near trees.',
+  element: ELEMENTS.NATURE,
+  tier: 0,
+  level: 5,
+  xp: 0,
+  nextLevelXp: 10,
+  statGrowthPerLevel: {
+    hp: calcStatGrowth(150, 0),
+    power: calcStatGrowth(0.65, 0),
+    defense: calcStatGrowth(10, 0),
+    speed: calcStatGrowth(0.5, 0),
+    special: calcStatGrowth(1.2, 0),
+  },
+  evolutionsList: [null, Mushiddle],
+  statsGrowthVisual: {
+    hp: 5,
+    defense: 3,
+    speed: 2,
+    special: 4,
+  },
+  stats: {
+    name: 'Mushyu',
+    hp: 25,
+    previousHp: 25,
+    maxHp: 25,
+    damage: 7,
+    defense: 6,
+    speed: 3,
+    special: 3,
+  },
+  spells: [FLOWER_POWER],
+  passiveSpells: [NATURES_SHIELD],
+}
+
+export const NATURE_MUSHROOM_MIDDLE = {
+  name: 'Mushiddle',
+  id: 'nature_mushroom_middle',
+  modelPath: '/models/mushroom-middle/mushroom-middle.fbx',
+  description: 'A nature fairy, known for playing around flowers.',
+  element: ELEMENTS.NATURE,
+  tier: 1,
+  level: 5,
+  xp: 0,
+  nextLevelXp: 10,
+  statGrowthPerLevel: {
+    hp: calcStatGrowth(230, 1),
+    power: calcStatGrowth(0.65, 1),
+    defense: calcStatGrowth(13, 1),
+    speed: calcStatGrowth(0.3, 1),
+    special: calcStatGrowth(0.8, 1),
+  },
+  evolutionsList: [NATURE_MUSHROOM_YOUNG, null],
+  statsGrowthVisual: {
+    hp: 5,
+    defense: 3,
+    speed: 2,
+    special: 4,
+  },
+  stats: {
+    name: 'Mushiddle',
+    hp: 25,
+    previousHp: 25,
+    maxHp: 25,
+    damage: 7,
+    defense: 6,
+    speed: 3,
+    special: 3,
+  },
+  spells: [FLOWER_POWER],
+  passiveSpells: [NATURES_SHIELD],
+}
+Mushiddle.value = NATURE_MUSHROOM_MIDDLE
+
+export default [NATURE_BUTTERFLY_MIDDLE, NATURE_MUSHROOM_YOUNG, NATURE_MUSHROOM_MIDDLE]
